987a1c3d0355b750b187b54b03f93806ae75487a
profflaviomoreni <profflavio.moreni@fiap.com.br>
2022-05-18 20:42:47 -0300

    Adicionado o data e model

---
+++ Formula1/Data/DriverRepository.cs
@@ -0,0 +1,90 @@
+﻿using Formula1.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Formula1.Data
+{
+    public class DriverRepository
+    {
+
+        public IList<Driver> FindAll()
+        {
+            return new List<Driver>
+            {
+                new Driver
+                {
+                    Name = "Sebastian Vettel",
+                    Team = "Ferrari",
+                    Country = "Germany",
+                    Points = 171,
+                    Podiums = 0,
+                    GrandPrixEnetered = 0,
+                    WorldChampionships = 4,
+                    HighestRaceFinish = "1 (x45)",
+                    HighestGridPosition = 1,
+                    DateOfBirth = new DateTime(1987,7,3),
+                    PlaceOfBirth = "Heppenheim, Germany",
+                    Bio="A tour de force as he swept to four straight world championship crowns and countless Formula One records, Sebastian Vettel’s relentless hunger for victory, as much as his outstanding talent, secure his place as one of the sport’s greats."
+
+                },
+
+                new Driver
+                {
+                    Name = "Lewis Hamilton",
+                    Team = "Mercedes",
+                    Country = "United Kingdom",
+                    Points = 151
+                },
+                new Driver
+                {
+                    Name = "Daniel Ricciardo",
+                    Team = "Red Bull Racing",
+                    Country = "Australia",
+                    Points = 107
+                },
+                new Driver
+                {
+                    Name = "Kimi Raikkonen",
+                    Team = "Ferrari",
+                    Country = "Finland",
+                    Points = 83
+                },
+                new Driver
+                {
+                    Name = "Sergio Perez",
+                    Team = "Force India",
+                    Country = "Mexico",
+                    Points = 45
+                },
+                new Driver
+                {
+                    Name = "Max Verstappen",
+                    Team = "Red Bull Racing",
+                    Country = "Netherlands",
+                    Points = 45
+                },
+                new Driver
+                {
+                    Name = "Esteban Ocon",
+                    Team = "Force India",
+                    Country = "France",
+                    Points = 39
+                },
+                new Driver
+                {
+                    Name = "Carlos Sainz",
+                    Team = "Toro Rosso",
+                    Country = "Spain",
+                    Points = 29
+                },
+                new Driver
+                {
+                    Name = "Felipe Massa",
+                    Team = "Williams",
+                    Country = "Brazil",
+                    Points = 22
+                },
+            };
+        }
+    }
+}
