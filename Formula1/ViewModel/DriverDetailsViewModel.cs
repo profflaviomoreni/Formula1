c444f4d00c9a19fd7acca83b210350fe3c07aa59
profflaviomoreni <profflavio.moreni@fiap.com.br>
2022-05-18 22:25:41 -0300

    adicionando a view de detalhes do piloto

---
+++ Formula1/ViewModel/DriverDetailsViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Formula1.Models;
+using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Formula1.ViewModel
+{
+    public class DriverDetailsViewModel : BaseViewModel
+    {
+
+        #region properties
+        private Driver driver;
+
+        public Driver Driver
+        {
+            get { return driver; }
+            set { SetProperty(ref driver, value); }
+        }
+        #endregion
+        public ICommand OpenBrowserCommand => new Command<Driver>(async (Driver d) =>
+        {
+            var uri = new Uri("https://www.f1.com");
+            await Xamarin.Essentials.Browser.OpenAsync(uri);
+        });
+
+        #region command
+
+
+        #endregion
+
+
+
+        #region construtor
+        public DriverDetailsViewModel()
+        {
+            driver = new Driver();
+        }
+
+        public DriverDetailsViewModel(Driver driver)
+        {
+            this.Driver = driver;
+        }
+        #endregion
+
+
+    }
+}
---
+++ Formula1/Views/DriveDetailsView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Formula1.ViewModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Formula1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DriveDetailsView : ContentPage
+    {
+        public DriveDetailsView(DriverDetailsViewModel driverDetailsViewModel)
+        {
+            InitializeComponent();
+            BindingContext = driverDetailsViewModel;
+        }
+    }
+}
